f4f4443a1c5f49e444820e079c1d2384a9ddfa85
Heric da Silva Cruz <hericsilvaho@gmail.com>
2024-10-26 16:18:41 -0300

    adicionando uma main

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,48 @@ use std::env;
 
 use graph::Graph;
 
-mod genetic_algorithm;
-pub mod graph;
+mod graph;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // Cria um novo grafo
+    let mut graph = Graph::new();
 
-    if args.len() < 2 {
-        eprintln!("Uso: {} <caminho do arquivo>", args[0]);
-        std::process::exit(1); // Sai com erro
+    // Adiciona vértices
+    graph.add_vertex("A".to_string());
+    graph.add_vertex("B".to_string());
+    graph.add_vertex("C".to_string());
+    graph.add_vertex("D".to_string());
+
+    // Adiciona arestas
+    graph.add_edge("A".to_string(), "B".to_string());
+    graph.add_edge("A".to_string(), "C".to_string());
+    graph.add_edge("B".to_string(), "D".to_string());
+
+    // Define rótulos
+    graph.set_label("A".to_string(), 0);
+    graph.set_label("B".to_string(), 2);
+    graph.set_label("C".to_string(), 0);
+    graph.set_label("D".to_string(), 1);
+
+    // Exibe o grafo
+    println!("Grafo:");
+    graph.display();
+
+    // Executa a procedure_h1
+    let result_h1 = graph.procedure_h1();
+    println!("\nResultado da Procedure H1:");
+    for (vertex, label) in result_h1 {
+        println!("Vertex: {}, Label: {}", vertex, label);
     }
 
-    let file_path = &args[1];
-
-    match Graph::from_file(file_path.to_string()) {
-        Ok(g) => {
-            print!("Grafo carregado com sucesso.");
-
-            g.display();
-        }
-
-        Err(e) => {
-            eprintln!("Erro ao carregar o grafo: {}", e);
-            std::process::exit(1); // Sai com erro
-        }
+    // Executa a procedure_h2
+    let result_h2 = graph.procedure_h2();
+    println!("\nResultado da Procedure H2:");
+    for (vertex, label) in result_h2 {
+        println!("Vertex: {}, Label: {}", vertex, label);
     }
+
+    // Verifica se o grafo é dominador romano
+    let is_roman = graph.is_roman_dominating_graph();
+    println!("\nO grafo é um grafo de dominação romana? {}", is_roman);
 }
