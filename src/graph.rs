cf2cd0162d6bf6eab796c66bb212ee5bf51c57ae
Heric da Silva Cruz <hericsilvaho@gmail.com>
2024-10-27 17:05:18 -0300

    modificando heuristicas h1 e h2 para serem compativeis com a nova representação de grafo

--- src/graph.rs
+++ src/graph.rs
@@ -8,7 +8,7 @@ pub struct Graph {
 }
 
 impl Graph {
-    fn new(num_vertices: usize, edges: &[(usize, usize)]) -> Self {
+    pub fn new(num_vertices: usize, edges: &[(usize, usize)]) -> Self {
         let mut adjacency_list = vec![vec![]; num_vertices];
         for &(u, v) in edges {
             adjacency_list[u].push(v);
@@ -30,64 +30,53 @@ impl Graph {
     pub fn get_vertex_degree(&self, vertex: usize) -> usize {
         self.adjacency_list[vertex].len()
     }
+
+    pub fn h1(&self) -> Vec<u8> {
+        let mut f: Vec<u8> = vec![0; self.adjacency_list.len()];
+        let mut unvisited: HashSet<usize> = (0..self.adjacency_list.len()).collect();
+        let mut rng = rand::thread_rng();
+
+        while !unvisited.is_empty() {
+            let &u = unvisited.iter().choose(&mut rng).unwrap();
+            f[u] = 2;
+            unvisited.remove(&u);
+
+            for &v in self.get_neighbors(u) {
+                f[v] = 0;
+                unvisited.remove(&v);
+            }
+
+            if unvisited.len() == 1 {
+                let &last = unvisited.iter().next().unwrap();
+                f[last] = 1;
+                unvisited.remove(&last);
+            }
+        }
+        f
+    }
+
+    pub fn h2(&self) -> Vec<u8> {
+        let mut f: Vec<u8> = vec![0; self.adjacency_list.len()];
+        let mut unvisited: Vec<usize> = (0..self.adjacency_list.len()).collect();
+
+        // Ordena por grau decrescente
+        unvisited.sort_by_key(|&vertex| std::cmp::Reverse(self.get_vertex_degree(vertex)));
+
+        while !unvisited.is_empty() {
+            let u = unvisited.remove(0);
+            f[u] = 2;
+
+            for &v in self.get_neighbors(u) {
+                f[v] = 0;
+                unvisited.retain(|&x| x != v);
+            }
+
+            if unvisited.len() == 1 {
+                let last = unvisited[0];
+                f[last] = 1;
+                unvisited.clear();
+            }
+        }
+        f
+    }
 }
-//
-//     pub fn procedure_h1(&self) -> HashMap<String, u8> {
-//         // `f` representa nossa função de dominação romana
-//         let mut f: HashMap<String, u8> = HashMap::new();
-//         let mut unvisited: HashSet<String> = self.adjacency_list.keys().cloned().collect();
-//         let mut rng = rand::thread_rng();
-//
-//         while !unvisited.is_empty() {
-//             // Seleciona um vértice `u` de `unvisited` aleatoriamente
-//             let u = unvisited.iter().choose(&mut rng).unwrap().clone();
-//
-//             // Atribui f(u) = 2 e remove `u` do conjunto `unvisited`
-//             f.insert(u.clone(), 2);
-//             unvisited.remove(&u);
-//
-//             // Para cada vizinho `v` de `u`, define f(v) = 0 e remove `v` de `unvisited`
-//             for v in self.neighbors(&u) {
-//                 f.insert(v.clone(), 0);
-//                 unvisited.remove(&v);
-//             }
-//
-//             if unvisited.len() == 1 {
-//                 let last_vertex = unvisited.iter().next().unwrap().clone();
-//                 f.insert(last_vertex.clone(), 1);
-//                 unvisited.remove(&last_vertex);
-//             }
-//         }
-//         f
-//     }
-//
-//     pub fn procedure_h2(&self) -> HashMap<String, u8> {
-//         let mut f: HashMap<String, u8> = HashMap::new();
-//         let mut unvisited: Vec<String> = self.adjacency_list.keys().cloned().collect();
-//
-//         // Ordena o vetor de forma decrescente de acordo com o tamanho da vizinça
-//         unvisited.sort_by_key(|vertex| {
-//             std::cmp::Reverse(
-//                 self.adjacency_list
-//                     .get(vertex)
-//                     .map_or(0, |neighbors| neighbors.len()),
-//             )
-//         }); // Usei clousures, para ordenar o vetor de unvisited pelo grau de cada vértice
-//
-//         while !unvisited.is_empty() {
-//             let u = unvisited.remove(0);
-//             f.insert(u.clone(), 2); // f(u) = 2
-//
-//             for v in self.neighbors(&u) {
-//                 f.insert(v.clone(), 0);
-//                 unvisited.retain(|x| x != &v);
-//             }
-//
-//             if unvisited.len() == 1 {
-//                 let last_vertex = unvisited[0].clone();
-//                 f.insert(last_vertex.clone(), 1);
-//                 unvisited.clear();
-//             }
-//         }
-//         f
-//     }
