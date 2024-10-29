5d6e3464489c44bb5e95d66555f169776b34fe32
Heric da Silva Cruz <hericsilvaho@gmail.com>
2024-10-29 16:41:01 -0300

    adicionando funções de crossover e run

--- src/genetic_algorithm.rs
+++ src/genetic_algorithm.rs
@@ -3,8 +3,8 @@ use std::{
     vec,
 };
 
+use rand::seq::SliceRandom;
 use rand::thread_rng;
-use rand::{seq::SliceRandom, Rng};
 
 use crate::graph::Graph;
 
@@ -67,7 +67,7 @@ impl RomanDominationGA {
         }
     }
 
-    pub fn generate_initial_population(&self) -> Vec<Solution> {
+    fn generate_initial_population(&self) -> Vec<Solution> {
         let mut population = vec![];
 
         let h2_solution = self.generate_h2_solution();
@@ -103,11 +103,11 @@ impl RomanDominationGA {
         Solution::new(labels, fitness)
     }
 
-    pub fn evaluate_fitness(&mut self, solution: &mut Solution) {
+    fn evaluate_fitness(&self, solution: &mut Solution) -> usize {
         if solution.fitness.is_none() {
             solution.fitness = Some(solution.labels.iter().map(|&x| x as usize).sum());
         }
-        solution.fitness.unwrap();
+        solution.fitness.unwrap()
     }
 
     fn is_feasible(&self, solution: &Solution) -> bool {
@@ -176,5 +176,91 @@ impl RomanDominationGA {
         }
 
         selected
+    }
+
+    fn crossover(&self, parent_a: &Solution, parent_b: &Solution) -> Solution {
+        let mut rng = thread_rng();
+        let (r1, r2) = {
+            let mut indices: Vec<usize> = (0..self.graph.get_num_vertices()).collect();
+            indices.shuffle(&mut rng);
+            (indices[0], indices[1])
+        };
+
+        let (r1, r2) = (r1.min(r2), r1.min(r1));
+        let mut child_labels = parent_a.labels.clone();
+        child_labels[r1..r2].copy_from_slice(&parent_b.labels[r1..r2]);
+        let mut child = Solution::new(child_labels, None);
+        self.evaluate_fitness(&mut child);
+
+        if !self.is_feasible(&child) {
+            self.make_feasible(&mut child);
+        }
+
+        child
+    }
+
+    pub fn run(
+        &mut self,
+        max_generations: usize,
+        max_stagnant: usize,
+        tournament_size: usize,
+        crossover_probability: f32,
+    ) -> Solution {
+        let mut population: Vec<Solution> = self.generate_initial_population();
+        population.iter_mut().for_each(|solution| {
+            self.evaluate_fitness(solution);
+        }); // Garante que todos os fitness foram calculados
+
+        let mut best_solution = population
+            .iter()
+            .min_by_key(|s| s.fitness.unwrap())
+            .unwrap()
+            .clone();
+
+        let mut stagnant_generations = 0; //Guarda o númeor de gerações sem que o fitness seja
+                                          //alterador
+
+        for _ in 0..max_generations {
+            if stagnant_generations >= max_stagnant {
+                break;
+            }
+
+            let intermediate_pop = self.tournament_selection(&mut population, tournament_size);
+            let mut new_pop = Vec::with_capacity(population.len());
+
+            for i in (0..intermediate_pop.len()).step_by(2) {
+                if rand::random::<f32>() < crossover_probability {
+                    if i + 1 < intermediate_pop.len() {
+                        let child = self.crossover(&intermediate_pop[i], &intermediate_pop[i + 1]);
+                        new_pop.push(child);
+                    }
+                } else {
+                    new_pop.push(intermediate_pop[i].clone());
+                    if i + 1 < intermediate_pop.len() {
+                        new_pop.push(intermediate_pop[i + 1].clone());
+                    }
+                }
+            }
+            population = new_pop;
+            population.iter_mut().for_each(|solution| {
+                self.evaluate_fitness(solution);
+            });
+
+            let current_best = population
+                .iter()
+                .min_by_key(|s| s.fitness.unwrap())
+                .unwrap()
+                .clone();
+
+            // Atualiza a melhor solução encontrada e verifica estagnação
+            if current_best.fitness < best_solution.fitness {
+                best_solution = current_best;
+                stagnant_generations = 0; // Reseta estagnação
+            } else {
+                stagnant_generations += 1;
+            }
+        }
+
+        best_solution
     }
 }
