1c951dab5a97ce4f152f251d0358093c49bed7ac
Heric da Silva Cruz <hericsilvaho@gmail.com>
2024-10-30 13:39:02 -0300

    atualizando main

--- src/main.rs
+++ src/main.rs
@@ -7,44 +7,65 @@ mod graph;
 use std::env;
 use std::path::Path;
 use std::process::exit;
+use std::time::Instant;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 7 {
-        eprintln!("Uso: {} <file_path> <population_size> <max_generations> <max_stagnant> <tournament_size> <crossover_probability>", args[0]);
+    if args.len() < 3 {
+        eprintln!("Uso: {} <file_path> <trials>", args[0]);
         exit(1);
     }
 
     let file_path = args[1].clone();
-    let population_size: usize = args[2].parse().expect("Erro ao ler population_size");
-    let max_generations: usize = args[3].parse().expect("Erro ao ler max_generations");
-    let max_stagnant: usize = args[4].parse().expect("Erro ao ler max_stagnant");
-    let tournament_size: usize = args[5].parse().expect("Erro ao ler tournament_size");
-    let crossover_probability: f32 = args[6].parse().expect("Erro ao ler crossover_probability");
 
+    const MAX_STAGNANT: usize = 10;
+    const GENERATIONS: usize = 1000;
+    const TOURNAMENT_SIZE: usize = 2;
+    const CROSSOVER_PROBABILITY: f32 = 0.9;
     let file_name = Path::new(&file_path)
         .file_stem()
         .and_then(|name| name.to_str())
         .unwrap_or("arquivo");
 
+    let trials: usize = match args[2].parse() {
+        Ok(n) => n,
+        Err(_) => {
+            eprintln!("Erro: 'trials' deve ser um número inteiro positivo.");
+            exit(1);
+        }
+    };
+
     let graph = Graph::from_file(file_path.to_string())?;
+    let graph_order = graph.get_num_vertices();
+    let graph_size = graph.get_graph_size();
+    let pop_size: usize = (graph.get_num_vertices() as f64 / 1.5).round() as usize;
 
-    let mut rdga = RomanDominationGA::new(graph, Some(population_size));
+    println!("graph_name,graph_order,graph_size,fitness_value,elapsed_time(microsecond)");
 
-    let best_solution = rdga.run(
-        max_generations,
-        max_stagnant,
-        tournament_size,
-        crossover_probability,
-    );
+    let mut rdga = RomanDominationGA::new(graph, Some(pop_size));
 
-    println!(
-        "{},{},{}",
-        file_name,
-        best_solution.fitness.unwrap(),
-        max_generations
-    );
+    for _ in 0..trials {
+        let start = Instant::now();
+        let solution = rdga.run(
+            GENERATIONS,
+            MAX_STAGNANT,
+            TOURNAMENT_SIZE,
+            CROSSOVER_PROBABILITY,
+        );
+        let end = Instant::now();
+        let elapsed_time = end.duration_since(start);
+        let elapsed_in_microseconds = elapsed_time.as_micros();
+
+        println!(
+            "{},{},{},{},{}",
+            file_name,
+            graph_order,
+            graph_size,
+            solution.fitness.unwrap_or_default(),
+            elapsed_in_microseconds
+        );
+    }
 
     Ok(())
 }
